97615da6bd1e42490e18d909b12c89e0770e51f1
Solidor <justin@geistnet.com>
2022-06-18 20:17:48 -0700

    Removed Boilerplate attrributes and CR

--- module/helpers/config.mjs
+++ module/helpers/config.mjs
@@ -4,23 +4,6 @@ export const TITAN = {};
  * The set of Ability Scores used within the sytem.
  * @type {Object}
  */
-TITAN.abilities = {
-  str: "TITAN.AbilityStr",
-  dex: "TITAN.AbilityDex",
-  con: "TITAN.AbilityCon",
-  int: "TITAN.AbilityInt",
-  wis: "TITAN.AbilityWis",
-  cha: "TITAN.AbilityCha",
-};
-
-TITAN.abilityAbbreviations = {
-  str: "TITAN.AbilityStrAbbr",
-  dex: "TITAN.AbilityDexAbbr",
-  con: "TITAN.AbilityConAbbr",
-  int: "TITAN.AbilityIntAbbr",
-  wis: "TITAN.AbilityWisAbbr",
-  cha: "TITAN.AbilityChaAbbr",
-};
 
 TITAN.attributes = {
   body: "TITAN.Attribute.Body",
